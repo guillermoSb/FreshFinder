336aaf32c63d3d6fa0bc0f751114d8e44f50e07a
Guillermo Santos <guillermo.santosba@gmail.com>
2023-03-19 14:58:51 -0600

    Added Grocery data structure to store the process.

---
+++ FreshFinder/Model/Grocery.swift
@@ -0,0 +1,15 @@
+//
+//  Grocery.swift
+//  FreshFinder
+//
+//  Created by Guillermo Santos Barrios on 3/19/23.
+//
+
+import Foundation
+
+
+struct Grocery {
+    let list: GroceryList
+    let itemsPurchased: [GroceryListItem]
+    let createdAt = Date()
+}
