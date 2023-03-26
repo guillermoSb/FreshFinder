a1bf4246eaf9a47ad38ef390420f316618615aeb
Guillermo Santos <guillermo.santosba@gmail.com>
2023-03-26 17:32:19 -0600

    Created Realm models for GroceryList and GroceryListItem

--- FreshFinder/Model/GroceryList.swift
+++ FreshFinder/Model/GroceryList.swift
@@ -6,21 +6,39 @@
 //
 
 import Foundation
+import RealmSwift
 
+//struct GroceryList: Hashable, Identifiable {
+//    var id: String {
+//        return UUID().uuidString
+//    }
+//    var name: String
+//    var items: [GroceryListItem]
+//}
+//
+//struct GroceryListItem: Hashable, Identifiable {
+//    var id: String {
+//        return UUID().uuidString
+//    }
+//    var name: String
+//    var quantity: Int
+//    var price: Double?
+//}
 
-struct GroceryList: Hashable, Identifiable {
-    var id: String {
-        return UUID().uuidString
-    }
-    var name: String
-    var items: [GroceryListItem]
+class GroceryList: Object {
+    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted var name: String = ""
+    @Persisted var createdAt = Date()
+    
+    // Items for this GroceryList
+    @Persisted var items: List<GroceryListItem>
 }
 
-struct GroceryListItem: Hashable, Identifiable {
-    var id: String {
-        return UUID().uuidString
-    }
-    var name: String
-    var quantity: Int
-    var price: Double?
+class GroceryListItem: Object {
+    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted var name: String = ""
+    @Persisted var quantity = 0
+    @Persisted var price: Double?
 }
+
+
